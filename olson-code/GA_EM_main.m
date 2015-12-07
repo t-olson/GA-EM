ee26f245cebcba2961f541799dd0dda2cdad9277
Tim Olson <tolson.timothy@gmail.com>
2015-12-07 16:39:54 -0500

    Major speed improvements, moved graph drawing outside of GA_EM (legacy version in GA_EM - animated)

--- olson-code/GA_EM_main.m
+++ olson-code/GA_EM_main.m
@@ -1,6 +1,13 @@
 function GA_EM_main()
 close all;
-SEED = 10; % change this to get different data sets
+SEED = 123; % change this to get different data sets
+
+% Set GA-EM Parameters
+R = 3; % number of EM steps for each GA iteration
+M = 15; % max number of components
+K = 6; % size of parent population
+H = 4;%floor(.8*K); % number of offspring
+p_m = 0.02; % mutation rate
 
 rng(SEED); % initialize sample data
 C = 5; % actual number of clusters
@@ -31,10 +38,13 @@ title(['Actual data and mixtures, MDL = ',  num2str(MDL_true)]);
 
 % Run EM algorithm for comparison
 rng(SEED); % reset random seed to ensure reproducibility
+disp('Running EM alone');
+tic
 Pop = InitPopulation(data, 15, C); % initialize (requires same M and K >= C to match GA-EM init)
-P = Pop(C); % only use the C^th one, which has correct components enabled
-EM_result = EM(P, data, 100);
+EM_result = Pop(C); % only use the C^th one, which has correct components enabled
+EM_result = EM(EM_result, data, Inf);
 MDL_EM = MDLencode(EM_result,data); % compute MDL value for EM result
+toc
 disp(['EM: ', num2str(MDL_EM)]); % print MDL value
 
 % plot EM-mixture
@@ -51,7 +61,28 @@ title(['Best EM Mixture, MDL = ',  num2str(MDL_EM)]);
 % Run GA_EM algorithm (plots of fits and MDL vs iteration are generated
 % internally)
 rng(SEED); % reset random seed
-[GA_EM_result, MDL_list] = GA_EM(data);
+disp('Running GA-EM');
+tic
+[GA_EM_result, MDL_list] = GA_EM(data, R, M, K, H, p_m);
+toc
 disp(['GA_EM: ', num2str(MDL_list(end))]); % print final MDL value
 
+% plot GA_EM-mixture
+subplot(2,2,3);
+scatter(data(:,1), data(:,2));
+hold on;
+index = 1:M;
+index = index(logical(GA_EM_result(1).code));
+for i=1:length(index)
+    Z = mvnpdf([X(:) Y(:)], GA_EM_result.means(:,index(i))', GA_EM_result.covs(:,:,index(i))); % compute Gaussian pdf
+    Z = reshape(Z,size(X));
+    contour(X,Y,Z,[.1,.1], 'LineColor', [0 1 0], 'LineWidth', 2);  % contour plot
+end
+title(['Best EM Mixture, MDL = ',  num2str(MDL_list(end))]);
+
+% show MDL plot
+subplot(2,2,4);
+hold off;
+plot(1:length(MDL_list), MDL_list(1:length(MDL_list)))
+title('MDL vs iterations');
 end
--- olson-code/Mutate.m
+++ olson-code/Mutate.m
@@ -1,23 +1,22 @@
-function newP = Mutate(P, data, p_m)
-newP = P;
+function P = Mutate(P, data, p_m)
 M = length(P(1).code);
 [~,d] = size(data);
 L = d + (d * (d + 1) / 2);
-meanRange = repmat(sqrt(var(data))',1,M);
-covData = cov(data);
+minData = repmat(min(data)', 1, M);
+maxData = repmat(max(data)', 1, M);
+% covData = cov(data);
 
 for i = 2:size(P,1) % skip best candidate
     % Randomly flip binary value
     mask = rand(1, M) < p_m;
-    newP(i).code(mask) = (~logical(newP(i).code(mask)));
+    P(i).code(mask) = (~logical(P(i).code(mask)));
     % reset weights to uniform with new code
-    newP(i).weights = zeros(1, M);
-    newP(i).weights(logical(newP(i).code)) = 1 / sum(newP(i).code);
+    P(i).weights = zeros(1, M);
+    P(i).weights(logical(P(i).code)) = 1 / sum(P(i).code);
     
     % mutate means
     mask = rand(d, M) < p_m / L;
-    shifts = (rand(size(mask(mask))) - 1/2) .* meanRange(mask);
-    newP(i).means(mask) = newP(i).means(mask) + shifts;
+    P(i).means(mask) = minData(mask) + (maxData(mask)-minData(mask)) .* rand(size(mask(mask)));
     
     % DON'T MUTATE COVARIANCES!
     % mutate covariances by changing eigenvalues to ensure it remains PD
