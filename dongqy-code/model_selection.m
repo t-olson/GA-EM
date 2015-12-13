a8e9c41fbf5727caf8d7f688577075a9ef2da9c9
dongqy <dongqy@umich.edu>
2015-12-13 13:36:05 -0500

    model selection figure changed

--- dongqy-code/model_selection.m
+++ dongqy-code/model_selection.m
@@ -9,12 +9,12 @@ true_M = 7;
 d = 2;
 if gen_data
     N = 300*true_M;
-    [X, true_mu, true_sigma] = SampleData(d, N, true_M);
+    [X, true_mu, true_sigma] = SampleData(d, N, true_M, 1.2);
 end
 
 % Run GA_EM on the data with different model parameters
 figure
-for p = 1:4
+for p = 1:6
     init_mode = 'random';  % Initialization mode : random or k-means
     K = 6;                  %   K : number of individual in the population
     H = 4;                  %   H : number of offsprings in the new population, assumed to be multiples of 2
@@ -30,8 +30,12 @@ for p = 1:4
     elseif p == 3       % H (H = pc*K)
         K = 20;
         para_array = [4,8,10,16,20];
-    elseif p == 4       % pm
+    elseif p == 4       % Mmax
+        para_array = [6,10,15,20,25];
+    elseif p == 5       % pm
         para_array = [0.001,0.02,0.005,0.1,0.3];
+    elseif p == 6       % t_corr
+        para_array = [0.7,0.8,0.9,0.95,0.98];
     end
         
     for i = 1:length(para_array)
@@ -41,20 +45,36 @@ for p = 1:4
             K = para_array(i);
         elseif p == 3       % H (H = pc*K)
             H = para_array(i);
-        elseif p == 4       % pm
+        elseif p == 4       % Mmax
+            Mmax = para_array(i);
+        elseif p == 5       % pm
             pm = para_array(i);
+        elseif p == 6       % t_corr
+            t = para_array(i);
         end
-        [~, ~, ~, mdl_array] = GA_EM(X, K, H, R, Mmax, t, pm, init_mode);
         
-        fprintf('i = %d\n', i);
+        [~, ~, ~, mdl_array] = GA_EM(X, K, H, R, Mmax, t, pm, init_mode);
+        fprintf('i = %d \n', i);
         mdl_array
         mdl_cell{i} = mdl_array;
+        
+%         mdl_cell{i} = zeros(1, 15);
+%         for j = 1:10        % Average over 10 indepedent runs
+%             [~, ~, ~, mdl_array] = GA_EM(X, K, H, R, Mmax, t, pm, init_mode);
+%             fprintf('i = %d, j = %d', i, j);
+%             mdl_array
+%             tmp = zeros(1,15);
+%             tmp(1,1:length(mdl_array)) = mdl_array;
+%             tmp(length(mdl_array)+1:end) = mdl_array(end);
+%             mdl_cell{i} = mdl_cell{i} + tmp;
+%         end
+%         mdl_cell{i} = mdl_cell{i}./10;
     end
 
     % Make plots
     symbol_array = ['s-';'+-';'*-';'x-';'o-'];
     legend_array = cell(1,length(para_array));
-    subplot(4, 1, p)
+    subplot(2, 3, p)
     hold on
     for i = 1:length(para_array)
         if p == 1           % R
@@ -63,8 +83,12 @@ for p = 1:4
             legend_array{i} = sprintf('K = %d', para_array(i));
         elseif p == 3       % H (H = pc*K)
             legend_array{i} = sprintf('H = %d', para_array(i));
-        elseif p == 4       % pm
+        elseif p == 4       % Mmax
+            legend_array{i} = sprintf('M_{max} = %d', para_array(i));
+        elseif p == 5       % pm
             legend_array{i} = sprintf('p_m = %.3f', para_array(i));
+        elseif p == 6       % t_corr
+            legend_array{i} = sprintf('t_{corr} = %.2f', para_array(i));
         end
 
         iter_array = 0:length(mdl_cell{i})-1;
