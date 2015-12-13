0739f64877933675777468b54b15d3ca423fbb44
dongqy <dongqy@umich.edu>
2015-12-13 00:37:17 -0500

    figures generated

--- dongqy-code/PD.m
+++ dongqy-code/PD.m
@@ -1,3 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Helper functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% A helper function to check if X is a positive define matrix
+
 function result = PD(X)
 [~,p] = chol(X);
 result = (~p);
