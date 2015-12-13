d58778f10cf08c3cd08d98ac991a7f69e395986c
Wenbo Shen <shenwb@umich.edu>
2015-12-13 10:31:01 -0500

    bug fixed in pendigit ksir

--- shen-code/Pendigit_KSIR.m
+++ shen-code/Pendigit_KSIR.m
@@ -74,4 +74,5 @@ gscatter(X2(:,1), X2(:,2),label_ts,[],'.',10);
 
 % dump the reduced feature and label to a .mat file
 X = X2;
-save('pendigit_ksir_2', 'X', 'label_ts');
+label = label_ts;
+save('pendigit_ksir_2', 'X', 'label');
