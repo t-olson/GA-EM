2228fe112bd695ae667b74ed145b3fd1aa3fc174
Wenbo Shen <shenwb@umich.edu>
2015-12-12 21:31:29 -0500

    KSIR applied on test data

--- shen-code/Dim_Red_KSIR.m
+++ shen-code/Dim_Red_KSIR.m
@@ -1,23 +1,33 @@
 % EECS 545 F15 Final project
 % Genetic-based EM Algorithm for Learning Gaussian Mixture Models
+
 % This script import the pendigits data from a .txt file
 % perform the PCA to reduce the feature dimension
 
 clear, clc
 
+rng(0);
 fileName = 'pendigits_all.txt';
 rawData = importdata(fileName);
 Ndigit = 5;
 
 % choose the digits from 0 to Ndigt-1
 X = rawData(rawData(:,17)< Ndigit,:);
-X = X(1:1000,:);
-% label for each entry
-label = X(:,17);
-% each entry is characterized by 16 features
-X = X(:,1:16);
-[n, d] = size(X);
+X = X(randperm(length(X)), :);
 
+% seperate data and label
+label = X(:,17);
+X = X(:,1:16);
+
+% seperate data into training and testing
+ntr = 1000;
+nts = 1000;
+X = X(1:ntr+nts, :);
+label = label(1:ntr+nts, :);
+
+
+% center and scale data
+[~, d] = size(X);
 % center and scale data
 for k=1:d
     xtemp = X(:,k);
@@ -27,34 +37,40 @@ for k=1:d
     X(:,k) = xtemp;
 end
 
+Xtr = X(1:ntr, :);
+Xts = X(ntr+1:end, :);
+label_tr = label(1:ntr, :);
+label_ts = label(ntr+1:end, :);
+
 % function handle to calculate gaussian kernel
 gs = @(x1, x2, s) exp(-dist2(x1, x2)/(2*s*s));
 
-% calculate the kernel matrix
-K = gs(X, X, 20);
-
-% center the kernel matrix
-A = ones(n, n) / n;
+% calculate the train-train kernel matrix
+K = gs(Xtr, Xtr, 20);
+% center the train-train kernel matrix
+A = ones(ntr, ntr) / ntr;
 K = K - 2*K*A - A*K*A;
 
-% perform the KSIR dimension reduction
+% perform the KSIR on traing data
 opts.pType = 'c';
 opts.H = 5;
-[SIR] = KSIR(K, label, 2, 0.1, opts);
+[SIR] = KSIR(K, label_tr, 2, 0.1, opts);
 
-% get the reduced data
-X2 = zeros(n, 2);
-for k=1:n
-    X2(k, :)=SIR.C*(K(:,k)-mean(K(:,k)))+SIR.b;
+% calculate the KxBar matrix
+Kx = gs(Xtr, Xts, 20);
+B = ones(ntr, nts) / ntr;
+Kx = Kx - A*Kx - K*B + A*K*B;
+
+% reduced dimensionality of test data
+X2 = zeros(nts, 2);
+for k=1:nts
+    X2(k, :)=SIR.C*(Kx(:,k)-mean(Kx(:,k)))+SIR.b;
 end
 
 % draw the clusters
 figure;
-gscatter(X2(:,1), X2(:,2),label,[],'.',10);
- 
+gscatter(X2(:,1), X2(:,2),label_ts,[],'.',10);
+
 % dump the reduced feature and label to a .mat file
 X = X2';
-ReadMe = ['X is the feature matrix.' ...
-          'X contains 1000 observations and each observation is 2 dimensional.'...
-          'label contains the labels (i.e. the real digit) of that observation'];
-save('pendigit_ksir_2', 'X', 'label', 'ReadMe');
+save('pendigit_ksir_2', 'X', 'label');
